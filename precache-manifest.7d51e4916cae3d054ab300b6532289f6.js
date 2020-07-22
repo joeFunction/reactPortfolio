505164a61b43fa5cf9c09ef597fb19d3de9d7712
Joe Adams <adams.jd10@gmail.com>
2020-07-22 12:41:47 -0700

    Updates

---
+++ precache-manifest.7d51e4916cae3d054ab300b6532289f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba20ba2b0ae30a4c0461c88fda10120b",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "5365a4ce8a17cc85d20a",
+    "url": "/reactportfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5365a4ce8a17cc85d20a",
+    "url": "/reactportfolio/static/js/2.351d2a95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactportfolio/static/js/2.351d2a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828db0daf878aa20501a",
+    "url": "/reactportfolio/static/js/main.880b0e6a.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  }
+]);
